a7ca8fcdd2fccbc8d535ad9b02ff18b13159bff7
daniel.rocha <danielsantosr.rj@gmail.com>
2017-03-10 17:30:02 -0300

    Cadastro de Animal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,13 +16,23 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('animalCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+.controller('animalCtrl', function ($scope, $stateParams, $http) {
+	
+	$scope.submeter = function(animal) {
+		
+		$http.post('http://localhost:3000/api/animais', animal, null)
+		.success(function(retorno) {
+			//$scope.mensagem = 'Animal cadastrado com sucesso';
+			console.log('Animal cadastrado com sucesso');
+			console.log(retorno);
+		})
+		.error(function(erro) {
+			console.log(erro);
+		});
+			
+	}
 
-
-}])
+})
    
 .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
